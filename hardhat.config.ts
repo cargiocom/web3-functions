b361f6bc2e07f227520bafea8041b24ede6eeaac
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-10-23 05:08:18 -0700

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@typechain/hardhat";
+
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@foundry-rs/hardhat-anvil";
