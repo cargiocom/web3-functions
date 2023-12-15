1502d6a6fab0633e11aa1c83db36d69a1a76d51d
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-12-15 01:51:57 -0800

    Update tenderly.ts

--- utils/tenderly.ts
+++ utils/tenderly.ts
@@ -5,7 +5,7 @@ const UrlBuilder = {
             const to = tos[i];
             const value = values[i];
             const data = datas[i];
-            const chainid = chainids[i];444
+            const chainid = chainids[i];4444
         }
     }
 };
